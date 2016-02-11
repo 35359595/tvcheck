34b77015835055d388a0400885221cb8de919f05
35359595 <35359595i@gmail.com>
2016-02-11 21:42:10 +0100

    hashSet?

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,10 @@ use hyper::Client;
 use hyper::header::Connection;
 use std::env;
 use std::path::PathBuf;
-
-
-//p.to_str().unwrap().to_owned(),
+use std::collections::HashSet;
 
 fn main() {
-	let mut homem = homedir();
+	let homem = homedir();
 	let home = &homem.to_str().unwrap();	
 
 	if !test(&home.to_string()) { match create_dir(&home.to_string()) {
@@ -22,6 +20,7 @@ fn main() {
 				Ok(_) => {},
 			} }
 	let mut listm = homedir();
+	listm.push(".tvcheck");
 	listm.push("list");
 
 	let list = &listm.to_str().unwrap();
@@ -34,26 +33,54 @@ fn main() {
 	
 	let vlist = read(&list.to_string());
 	for path in vlist {
-	if &path != "" {
-		let file = 	&path.trim_left_matches("http://fs.to/flist/").to_string();
-		let mut file = match File::create(file){
-			Ok(file) => file,
-			Err(_) => panic!("Unable to create file!"),
-		};
-		println!("List entry: {}", &path);
-		let season = get(&path);
-		for e in season {
-		match file.write_all(&e.as_bytes()){
-			Ok(file) => file,
-			Err(_) => panic!("Unable to write to file!"),
-		};
-		match file.write_all(b"\n"){
-			Ok(file) => file,
-			Err(_) => panic!("fucking new line!"),
+	 if &path != "" {
+		let file = &path.trim_left_matches("http://fs.to/flist/").to_string();
+		let mut filem = homedir();
+		filem.push(".tvcheck");
+		filem.push(file);
+		let target = &filem.to_str().unwrap();
+
+		//check watched series
+		//if not watched before
+		if !test(&target.to_string()) {
+			let mut file = match File::create(&filem){
+				Ok(file) => file,
+				Err(_) => panic!("Unable to create file!"),
+			};
+			println!("List entry: {}", &path);
+			let season = get(&path);
+			
+			//Writing new series to file
+	
+			for e in season {
+				match file.write_all(&e.as_bytes()){
+					Ok(file) => file,
+					Err(_) => panic!("Unable to write to file!"),
+				};
+				match file.write_all(b"\n"){
+					Ok(file) => file,
+					Err(_) => panic!("fucking new line!"),
+				};
+			
+				println!("Added new Episode: {}", e);
+			}
 		}
-		println!("Episode: {}", e);
+		
+		//if watched - checking series list
+		if test(&target.to_string()) {
+			let season = get(&path);
+			let local_series = read(target);
+			let remote: HashSet<_> = season.iter().cloned().collect();
+			let local: HashSet<_> = local_series.iter().cloned().collect();
+			for d in local.difference(&remote) {
+				println!("Difference!: {}", d);
+			}
 		}
-	}}
+		
+		else {
+			println!("Series allready in list!");
+		     }
+	} }
 }
 
 fn get(list: &str) -> Vec<String>{
@@ -61,8 +88,7 @@ fn get(list: &str) -> Vec<String>{
 	let mut responce = client.get(list).header(Connection::close()).send().unwrap();
 	let mut body = String::new();	
 	responce.read_to_string(&mut body).unwrap();
-	write(body, "episodes.txt");
-	read("episodes.txt")
+	body.lines().map(|s| s.to_owned()).collect::<Vec<_>>()
 }
 
 fn read(name: &str) -> Vec<String>{
@@ -75,8 +101,7 @@ fn read(name: &str) -> Vec<String>{
 		Ok(file) => file,
 		Err(_) => panic!("Unable to read from file!"),
 		};
-	let ret = eps.lines().map(|s| s.to_owned()).collect::<Vec<_>>();
-	ret
+	eps.lines().map(|s| s.to_owned()).collect::<Vec<_>>()
 }
 
 fn write(inpt: String, name: &str){
